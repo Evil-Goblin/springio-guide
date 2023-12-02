24cfd544ab876c9929cd13e43d11fa501ea89942
ForExample <jyh6197@gmail.com>
2023-12-02 17:55:22 +0900

    feat: SpringDataMongo with REST Sample Project Initialize

---
+++ AccessingMongoDBDatawithREST/src/main/java/hello/accessingmongodbdatawithrest/AccessingMongoDbDatawithRestApplication.java
@@ -0,0 +1,13 @@
+package hello.accessingmongodbdatawithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingMongoDbDatawithRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingMongoDbDatawithRestApplication.class, args);
+    }
+
+}
---
+++ AccessingMongoDBDatawithREST/src/test/java/hello/accessingmongodbdatawithrest/AccessingMongoDbDatawithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.accessingmongodbdatawithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMongoDbDatawithRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
