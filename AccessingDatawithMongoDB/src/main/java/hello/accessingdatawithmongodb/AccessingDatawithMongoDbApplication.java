7f4bcfde5e5d0f0458b88eeeb665bd44c95db84f
ForExample <jyh6197@gmail.com>
2023-12-02 16:46:47 +0900

    feat: SpringDataMongo Sample Project Initialize

---
+++ AccessingDatawithMongoDB/src/main/java/hello/accessingdatawithmongodb/AccessingDatawithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package hello.accessingdatawithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDatawithMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDatawithMongoDbApplication.class, args);
+    }
+
+}
