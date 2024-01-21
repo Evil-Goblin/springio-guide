7d1fa8f68b3771d9a375b921b763f127a98af7cb
ForExample <jyh6197@gmail.com>
2024-01-21 22:46:29 +0900

    feat: Add Building Web Application With SpringBoot And Kotlin

---
+++ BuildingWebApplicationsWithSpringBootAndKotlin/src/test/kotlin/hello/buildingwebapplicationswithspringbootandkotlin/BuildingWebApplicationsWithSpringBootAndKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.buildingwebapplicationswithspringbootandkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuildingWebApplicationsWithSpringBootAndKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
